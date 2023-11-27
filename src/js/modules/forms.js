ae1904059bd3769eeafd815edd4a868c59450140
mher-777 <mherjan2003@mail.ru>
2023-11-27 20:00:06 +0500

    Доверстал секцию Обратную связь

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,26 +1,47 @@
 import Inputmask from "inputmask";
-import validate from "jquery-validation";
+import validate from "../vendor/jquery-validation"
 
 var forms = {
 	mask: () => {
-		var selector = document.querySelectorAll("input[name='phone']");
+		var selector = document.querySelectorAll("input.js-mask-phone");
+		var email = document.querySelectorAll("input.js-mask-email");
 
 		var im = new Inputmask({
 			mask: "+7 (999) 999-99-99",
 			clearMaskOnLostFocus: true,
 			clearIncomplete: false,
+			showMaskOnHover: false,
+			inputmode: 'tel'
+		});
+
+		var emailMask = new Inputmask({
+			mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+			greedy: false,
+			showMaskOnHover: false,
+			inputmode: 'email',
+			onBeforePaste: function (pastedValue, opts) {
+				pastedValue = pastedValue.toLowerCase();
+				return pastedValue.replace("mailto:", "");
+			},
+			definitions: {
+				'*': {
+					validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+					casing: "lower"
+				}
+			}
 		});
 
 		im.mask(selector);
+		emailMask.mask(email);
 	},
 
 	validate: () => {
-		$("form").each((i, el) => {
+		$("form.js-validate").each((i, el) => {
 			var $form = $(el);
-
 			$form.validate({
 				errorPlacement: function(error, element) {
-					$(element).closest('.input').append(error)
+					$(element).closest('.input').find('.input__parent').append(error.attr('title', error.text()).hide()
+						.fadeIn(500))
 				},
 				highlight: (element, errorClass, validClass) => {
 					$(element)
